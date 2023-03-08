d1477978581a7bb6c833702d373c6dbef4d9ca20
shacketh <81651297+shacketh@users.noreply.github.com>
2023-03-08 20:12:45 -0300

    Create PrimeiroProjeto.sol
    Hello!

---
+++ PrimeiroProjeto.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract PrimeiroProjeto {
+
+    event NovaConsulta(uint idConsulta, string nome, string mensagem, uint numeroDaSorte);
+
+    string datadenascimento = "28/02/2023";
+
+    struct QuemMeConsulta {
+        string nome;
+        string mensagem;
+    }
+    
+    QuemMeConsulta[] public consultas;
+
+    function QuandoIniciou() public view returns (string memory) {
+        return datadenascimento;
+    }
+
+    function FazerConsulta(string memory _nome, string memory _mensagem) public returns (uint) {
+        consultas.push(QuemMeConsulta(_nome, _mensagem));
+        uint idConsulta = consultas.length - 1;
+        uint random = uint(keccak256(abi.encodePacked(_mensagem)));
+        emit NovaConsulta(idConsulta, _nome, _mensagem, random);
+        return random;
+    }
+
+    function ConsultaAnonima(string memory _mensagem) public {
+        FazerConsulta("anonimo", _mensagem);
+    }
+}
